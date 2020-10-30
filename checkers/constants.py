f7139f30e0fdde68ed2124168546789b0c4136f6
dkeen10 <duncankeen@gmail.com>
2020-10-29 18:35:07 -0700

    created pygame window

---
+++ checkers/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+WIDTH = 800
+HEIGHT = 800
+ROWS = 8
+COLUMNS = 8
+SQUARE_SIZE = WIDTH // COLUMNS
+
+# colours:
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from checkers.constants import WIDTH, HEIGHT
+
+FPS = 60
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Checkers")
+
+
+def main():
+    run = True
+
+    # setting framerat:
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        # checking events types:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
