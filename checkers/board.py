e7401ff7f5810ee5b924d6d04520807430b711e2
dkeen10 <duncankeen@gmail.com>
2020-10-29 18:49:22 -0700

    created board

---
+++ checkers/board.py
@@ -0,0 +1,18 @@
+import pygame
+from .constants import BLACK, RED, ROWS, SQUARE_SIZE
+
+
+class Board:
+    def __init__(self):
+        self.board = []
+        self.selected_piece = None
+        self.red_remaining = 12
+        self.white_remaining = 12
+        self.red_kings = 0
+        self.white_kings = 0
+
+    def draw_squares(self, window):
+        window.fill(BLACK)
+        for row in range(ROWS):
+            for column in range(row % 2, ROWS, 2):
+                pygame.draw.rect(window, RED, (row * SQUARE_SIZE, column * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
